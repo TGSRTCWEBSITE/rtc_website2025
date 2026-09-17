2f6bd0881b566783afd141c7d127138bb6e9537d
TGSRTCWEBSITE <tgwebsiteho@gmail.com>
2026-09-17 14:49:09 +0530

    Update index.tsx
    autoplayspeed changed 3000 to 2000

--- components/home-banner-carousal/index.tsx
+++ components/home-banner-carousal/index.tsx
@@ -56,7 +56,7 @@ const HomeBannerAnimation: React.FC<CarouselInterface> = ({
     // We set the infinity prop to false to resolve the duplicate banner issue.
     infinite: true,
     autoplay: autoplay,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2000,
     speed: 1000,
     slidesToShow: perView,
     slidesToScroll: perView,
